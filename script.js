e85bf0d3564d05dc8c63530f1884380f5d629150
gavin <gavin.lim@alumni.ubc.ca>
2022-01-06 19:29:18 -0800

    Add function to display books in array

--- script.js
+++ script.js
@@ -14,13 +14,55 @@ book.prototype.info = function() {
     return out;
 }
 
-function add_book_to_library() {
+// Open pop-up form
+function open_form() {
+    document.getElementById('form').style.display = "block";
+    console.log('hello');
+}
 
-    // Prompt for user input 
+// Close pop-up form
+function close_form() {
+    document.getElementById('form').style.display = "none";
+}
 
-    // Store new book object into my_library array 
+// Store new book object into my_library array 
+function add_to_library(input_book) {    
+    my_library.push(input_book);
+}
+
+function foo(book) {
+    
 
 }
 
-// const lotr = new book("The Hobbit", "J.R.R. Tolkien", 295, true);
-// console.log(lotr.info());
+function read_library() {
+    let parent = document.getElementsByClassName('library')[0];
+    // const card = document.createElement('div');
+    // parent.appendChild(card);
+
+    // Add to library grid
+    my_library.forEach(book => {
+        const child = document.createElement('div');
+        child.classList.add('book');
+
+        for (const [key, value] of Object.entries(book)) {
+            console.log(`${key}: ${value}`);
+            const paragraph = document.createElement('p');
+            paragraph.innerHTML = `${key}: ${value}`;
+            child.appendChild(paragraph);
+        }
+
+        // child.innerHTML = book.title;
+        parent.appendChild(child);
+    });
+}
+
+const lotr = new book("The Hobbit", "J.R.R. Tolkien", 295, true);
+const harrypotter = new book("Globet of Fire", "J.K. Rowling", 200, true);
+const starwars = new book("Dark Force Rising", "Timothy Zahn", 34, false);
+
+add_to_library(lotr);
+add_to_library(harrypotter);
+add_to_library(starwars);
+read_library();
+
