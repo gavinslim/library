c62bcce285cc39c9a7dd7707212982475885cd7d
gavin <gavin.lim@alumni.ubc.ca>
2022-01-07 22:28:07 -0800

    Add hover button display for books

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let my_library = [];
 
+// Retrieve selectors for form (data attributes) and overlay
+// Reference: https://www.youtube.com/watch?v=MBaw_6cPmAw&ab_channel=WebDevSimplified
+const open_button = document.querySelector('[data-open-button]');
+const close_button = document.querySelector('[data-close-button]');
+const submit_button = document.querySelector('[data-submit-button]');
+const overlay = document.getElementById('overlay');
+
 function book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -14,6 +21,38 @@ book.prototype.info = function() {
     return out;
 }
 
+// Store new book object into my_library array 
+function add_to_library(input_book) {    
+    my_library.push(input_book);
+}
+
+// Refresh library display
+function refresh_library() {
+    let parent = document.getElementsByClassName('library')[0];
+
+    // Delete current display
+    parent.innerHTML = '';
+
+    // Add to library grid
+    my_library.forEach(book => {
+    
+        const child = document.createElement('div');
+        child.classList.add('book');
+
+        const close = document.createElement('button');
+        close.classList.add('delete-btn');
+        child.appendChild(close);
+
+        for (const [key, value] of Object.entries(book)) {
+            const paragraph = document.createElement('p');
+            paragraph.innerHTML = `${value}`;
+            child.appendChild(paragraph);
+        }
+
+        parent.appendChild(child);
+    });
+}
+
 // Open pop-up form
 function open_form() {
     document.getElementById('form').style.display = "block";
@@ -25,44 +64,48 @@ function close_form() {
     document.getElementById('form').style.display = "none";
 }
 
-// Store new book object into my_library array 
-function add_to_library(input_book) {    
-    my_library.push(input_book);
-}
+// Activate form and overlay
+open_button.addEventListener('click', () => {
+    const form = document.querySelector('.form');
+    form.classList.add('active');
+    overlay.classList.add('active');
+});
 
-function foo(book) {
+// Disable form and overlay
+close_button.addEventListener('click', () => {
+    const form = document.querySelector('.form');
+    form.classList.remove('active');
+    overlay.classList.remove('active');
+});
+
+// Submit form
+submit_button.addEventListener('click', () => {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').value;
+
+    // Check if submitted book already exists in library
     
 
-}
+    // Add to library
+    let temp = read == 'on' ? true : false;
+    const new_book = new book(title, author, pages, temp);
+    add_to_library(new_book);
+    
+    // Modify feature of book if already read 
 
-function read_library() {
-    let parent = document.getElementsByClassName('library')[0];
-    // const card = document.createElement('div');
-    // parent.appendChild(card);
+    refresh_library();
+});
 
-    // Add to library grid
-    my_library.forEach(book => {
-        const child = document.createElement('div');
-        child.classList.add('book');
+// const lotr = new book("The Hobbit", "J.R.R. Tolkien", 295, true);
+// const harrypotter = new book("Globet of Fire", "J.K. Rowling", 200, true);
+// const starwars = new book("Dark Force Rising", "Timothy Zahn", 34, false);
+// const book1 = new book("Book 1", "Author 1", 123, true);
 
-        for (const [key, value] of Object.entries(book)) {
-            console.log(`${key}: ${value}`);
-            const paragraph = document.createElement('p');
-            paragraph.innerHTML = `${key}: ${value}`;
-            child.appendChild(paragraph);
-        }
-
-        // child.innerHTML = book.title;
-        parent.appendChild(child);
-    });
-}
-
-const lotr = new book("The Hobbit", "J.R.R. Tolkien", 295, true);
-const harrypotter = new book("Globet of Fire", "J.K. Rowling", 200, true);
-const starwars = new book("Dark Force Rising", "Timothy Zahn", 34, false);
-
-add_to_library(lotr);
-add_to_library(harrypotter);
-add_to_library(starwars);
-read_library();
+// add_to_library(lotr);
+// add_to_library(harrypotter);
+// add_to_library(starwars);
+// add_to_library(book1);
+// refresh_library();
 
